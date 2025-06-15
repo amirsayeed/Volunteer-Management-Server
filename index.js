a187d27f365cb3327e0723338fb0d8de93f1f9b5
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-06-16 02:52:38 +0600

    set firebase admin service key to an environment variable and deployed to vercel

--- index.js
+++ index.js
@@ -61,15 +61,14 @@ const client = new MongoClient(uri, {
 
 var admin = require("firebase-admin");
 
-// const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8');
-// const serviceAccount = JSON.parse(decoded);
-
-var serviceAccount = require("./firebase-admin-service-key.json");
-
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
+// var serviceAccount = require("./firebase-admin-service-key.json");
 
 
 async function run() {
